60f805b95290ef2542364b4e22061dba43b20ac3
Igor Buzykin <lexemz@gmail.com>
2021-10-10 20:15:18 +0300

    TasksTableVC - adding tasks in group

--- To-Do List/ViewControllers/TasksTableViewController.swift
+++ To-Do List/ViewControllers/TasksTableViewController.swift
@@ -8,9 +8,8 @@
 import RealmSwift
 
 class TasksTableViewController: UITableViewController {
-    
     var taskGroup: TaskGroup!
-    
+
     private var activeTasks: Results<Task>!
     private var completedTasks: Results<Task>!
 
@@ -18,13 +17,33 @@ class TasksTableViewController: UITableViewController {
         super.viewDidLoad()
 
         title = taskGroup.groupName
-        
+
         activeTasks = taskGroup.tasks.filter("isComplete = false")
         completedTasks = taskGroup.tasks.filter("isComplete = true")
     }
 
-    // MARK: - Table view data source
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = CustomAlert.createCustomAlert(title: "Add task", subtitle: "What do you want to add?")
+        
+        alert.addTaskAction { taskTitle, taskNote in
+            self.saveNewTask(taskTitle: taskTitle, taskNote: taskNote)
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func saveNewTask(taskTitle: String, taskNote: String?) {
+        let newTask = Task(value: [taskTitle, taskNote])
+        StorageManager.shared.save(task: newTask, to: taskGroup)
+        
+        let rowIndex = IndexPath(row: activeTasks.index(of: newTask) ?? 0, section: 0)
+        tableView.insertRows(at: [rowIndex], with: .automatic)
+    }
+}
 
+// MARK: - Table view data source
+
+extension TasksTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
@@ -32,12 +51,11 @@ class TasksTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         section == 0 ? activeTasks.count : completedTasks.count
     }
-    
+
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         section == 0 ? "Active Tasks" : "Completed Tasks"
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
         let task = indexPath.section == 0 ? activeTasks[indexPath.row] : completedTasks[indexPath.row]
@@ -45,14 +63,9 @@ class TasksTableViewController: UITableViewController {
         var content = cell.defaultContentConfiguration()
         content.text = task.name
         content.secondaryText = task.note
-        
+
         cell.contentConfiguration = content
-        
+
         return cell
     }
-
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-    }
-    
 }
