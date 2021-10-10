fad3e99e3886fc05eeb568b2b51d9f6016d172c1
Igor Buzykin <lexemz@gmail.com>
2021-10-10 16:34:05 +0300

    Created DataManager

--- To-Do List/Services/DataManager.swift
+++ To-Do List/Services/DataManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class DataManager {
+    static let shared = DataManager()
+    
+    private init() {}
+    
+    func createDemoData(_ completion: @escaping () -> Void) {
+        if !UserDefaults.standard.bool(forKey: "createdDemoData") {
+            UserDefaults.standard.set(true, forKey: "createdDemoData")
+            
+            let shoppingGroup = TaskGroup()
+            shoppingGroup.groupName = "Shopping"
+            
+            let milkTask = Task()
+            milkTask.name = "Milk"
+            milkTask.note = "2 Liters"
+            
+            shoppingGroup.tasks.append(milkTask)
+            
+            let breadTask = Task(value:["Bread", "", Date(), true]) // массив
+            let applesTask = Task(value: ["name": "Apples", "note": "2KG"]) // словарь
+            
+            shoppingGroup.tasks.insert(contentsOf: [breadTask, applesTask], at: 1) // массив сущностей и индекс вставки
+        }
+    }
+}
