168f9016696995f827e499193dbe5d8be3ca0da2
Igor Buzykin <lexemz@gmail.com>
2021-10-10 19:32:39 +0300

    StorageManager update

--- To-Do List/Services/StorageManager.swift
+++ To-Do List/Services/StorageManager.swift
@@ -14,9 +14,27 @@ class StorageManager {
     
     private init() {}
     
-    func saveContext(_ taskGroups: [TaskGroup]) {
-        try! realm.write {
+    // сохранение сущностей в Persistent
+    func save(_ taskGroups: [TaskGroup]) {
+        write {
             realm.add(taskGroups)
+        }
+    }
+    
+    func save(_ taskGroup: TaskGroup) {
+        write {
+            realm.add(taskGroup)
+        }
+    }
+    
+    // безопасное чтение БД
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch let error {
+            print(error)
         }
     }
 }
